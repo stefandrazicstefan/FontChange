8e43871373464d89d39152ecbd36cc2ecc9a1bef
StefiThe Bog <stefandrazicstefan@gmail.com>
2022-11-27 20:07:03 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace MenjanjeFonta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(253, 186);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 106);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Izaberi font";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(684, 470);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Font";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenjanjeFonta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string FONT;
+        
+        private void ExecuteFont(string font)
+        {
+            string PATH = Environment.CurrentDirectory;
+            PATH += "\\Font.reg";
+            using (FileStream fs = File.Create(PATH)) ;
+            using (StreamWriter fw = new StreamWriter(PATH))
+            {
+                fw.WriteLine("Windows Registry Editor Version 5.00");
+                fw.WriteLine("[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts]");
+                fw.WriteLine("\"Segoe UI(TrueType)\" = \"\"");
+                fw.WriteLine("\"Segoe UI Bold(TrueType)\" = \"\"");
+                fw.WriteLine("\"Segoe UI Bold Italic (TrueType)\" = \"\"");
+                fw.WriteLine("\"Segoe UI Italic(TrueType)\" = \"\"");
+                fw.WriteLine("\"Segoe UI Light(TrueType)\" = \"\"");
+                fw.WriteLine("\"Segoe UI Semibold(TrueType)\" = \"\"");
+                fw.WriteLine("\"Segoe UI Symbol(TrueType)\" = \"\"");
+                fw.WriteLine("[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\FontSubstitutes]");
+                fw.WriteLine("\"Segoe UI\" = \"{0}\"", font);
+                fw.Close();
+            }
+            Process.Start(PATH);
+            MessageBox.Show("Uspesno menjanje fonta!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowColor = true;
+
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                FONT = Convert.ToString(fontDialog1.Font.Name);
+                ExecuteFont(FONT);
+            }
+        }
+    }
+}
